2a5c98bee1118a4340bd0d19cfefadcb57f33d87
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-10 13:05:08 -0300

    feat: initial project structure

---
+++ src/main/java/br/com/omnirent/OmnirentApplication.java
@@ -0,0 +1,13 @@
+package br.com.omnirent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnirentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnirentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/omnirent/OmnirentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.omnirent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnirentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
