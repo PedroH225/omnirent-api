c66a66b7497d41d2cbc630dfc1a329aeee979734
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-12 15:51:48 -0300

    feat: add User model

---
+++ src/main/java/br/com/omnirent/user/model/User.java
@@ -0,0 +1,22 @@
+package br.com.omnirent.user.model;
+
+import br.com.omnirent.common.model.NamedEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@Table(name = "users")
+public class User extends NamedEntity {
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+
+	private String email;
+
+	private String password;
+	
+}
