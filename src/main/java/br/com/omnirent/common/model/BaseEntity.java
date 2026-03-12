78fee874973f95fbacaf427d3940b3e2eec5e746
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-12 15:09:50 -0300

    feat: add BaseEntity model

---
+++ src/main/java/br/com/omnirent/common/model/BaseEntity.java
@@ -0,0 +1,39 @@
+package br.com.omnirent.common.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	protected String id;
+	
+	@CreatedDate
+	protected LocalDateTime createdAt;
+	
+	@LastModifiedDate
+	protected LocalDateTime updatedAt;
+
+}
+	
