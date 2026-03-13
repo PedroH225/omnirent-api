abc1606731fa951e9e1b01495c1a761c799f38d6
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-13 16:40:19 -0300

    feat: map relationship for Item and Rental

--- src/main/java/br/com/omnirent/item/model/Item.java
+++ src/main/java/br/com/omnirent/item/model/Item.java
@@ -1,16 +1,19 @@
 package br.com.omnirent.item.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import br.com.omnirent.adress.model.Address;
 import br.com.omnirent.common.model.NamedEntity;
 import br.com.omnirent.item.enums.ItemCondition;
+import br.com.omnirent.rental.model.Rental;
 import br.com.omnirent.user.model.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -40,4 +43,7 @@ public class Item extends NamedEntity {
 	@ManyToOne
 	@JoinColumn(name = "owner_id")
 	private User owner;
+	
+	@OneToMany(mappedBy = "item")
+	private List<Rental> rentals;
 }
--- src/main/java/br/com/omnirent/rental/model/Rental.java
+++ src/main/java/br/com/omnirent/rental/model/Rental.java
@@ -4,11 +4,14 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import br.com.omnirent.common.model.BaseEntity;
+import br.com.omnirent.item.model.Item;
 import br.com.omnirent.rental.enums.RentalPeriod;
 import br.com.omnirent.rental.enums.RentalStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -31,6 +34,10 @@ public class Rental extends BaseEntity {
 	
 	@Enumerated(EnumType.STRING)
 	private RentalPeriod rentalPeriod;
+	
+	@ManyToOne
+	@JoinColumn(name = "item_id")
+	private Item item;
 	
 
 }
