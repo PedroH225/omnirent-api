29f532225dcec72b33f6470625402ec915123ec9
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-13 18:37:54 -0300

    feat: add initial Address layers

---
+++ src/main/java/br/com/omnirent/address/AddressRepository.java
@@ -0,0 +1,10 @@
+package br.com.omnirent.address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, String> {
+
+}
