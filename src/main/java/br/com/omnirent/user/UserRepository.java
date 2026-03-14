a5ab1cc1c3cd6a3ca5abbf98acd199b92c180d80
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-14 16:10:18 -0300

    feat: implement find user by email methods

--- src/main/java/br/com/omnirent/user/UserRepository.java
+++ src/main/java/br/com/omnirent/user/UserRepository.java
@@ -1,9 +1,19 @@
 package br.com.omnirent.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
+	UserDetails findByEmail(String email);
+	
+	Optional<User> findByEmailAndIdNot(String email, String id);
+	
+	@Query("SELECT u FROM User u WHERE u.email = :email")
+	Optional<User> findExistingUserByEmail(String email);
 }
