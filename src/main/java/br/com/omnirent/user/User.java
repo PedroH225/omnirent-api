5262865b938e67f457356e6b2c223762cf066fb0
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-14 14:58:44 -0300

    feat: add user authentication methods

--- src/main/java/br/com/omnirent/user/User.java
+++ src/main/java/br/com/omnirent/user/User.java
@@ -1,7 +1,11 @@
 package br.com.omnirent.user;
 
 import java.sql.Date;
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import br.com.omnirent.address.Address;
 import br.com.omnirent.common.NamedEntity;
@@ -21,7 +25,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @Entity
 @Table(name = "users")
-public class User extends NamedEntity {
+public class User extends NamedEntity implements UserDetails {
 	private static final long serialVersionUID = 1L;
 	
 	private String username;
@@ -43,5 +47,40 @@ public class User extends NamedEntity {
 	
 	@OneToMany(mappedBy = "renter")
 	private List<Rental> rented;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+	
+	@Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 	
 }
