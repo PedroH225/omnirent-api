08a0ad633c643c01e6081f210776299f9a2a1060
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-16 15:31:40 -0300

    feat: add User Status default string values

--- src/main/java/br/com/omnirent/common/enums/UserStatus.java
+++ src/main/java/br/com/omnirent/common/enums/UserStatus.java
@@ -1,7 +1,18 @@
 package br.com.omnirent.common.enums;
 
 public enum UserStatus {
-	ACTIVE,
-	INACTIVE,
-	BANNED;
+	ACTIVE("Active"),
+	INACTIVE("Inactive"),
+	BANNED("Banned");
+	
+	private String userStatus;
+	
+	UserStatus(String userStatus) {
+		this.userStatus = userStatus;
+	}
+	
+	@Override
+	public String toString() {
+		return this.userStatus;
+	}
 }
