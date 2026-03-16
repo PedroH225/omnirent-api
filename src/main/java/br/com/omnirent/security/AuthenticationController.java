ca63f408a63563f19e2357bb5a67bdea77b9afb4
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-16 13:43:27 -0300

    fix: correct auth endpoints mapping

--- src/main/java/br/com/omnirent/security/AuthenticationController.java
+++ src/main/java/br/com/omnirent/security/AuthenticationController.java
@@ -5,12 +5,14 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-@RestController("/auth")
+@RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
 	
 	private AuthenticationService authenticationService;
