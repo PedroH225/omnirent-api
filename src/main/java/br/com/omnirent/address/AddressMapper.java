8925cbf9f250fa51005fe8443124f8365aac0110
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-17 15:54:14 -0300

    feat: implement to single DTO and from request Dto functions

--- src/main/java/br/com/omnirent/address/AddressMapper.java
+++ src/main/java/br/com/omnirent/address/AddressMapper.java
@@ -10,4 +10,22 @@ public class AddressMapper {
 				.map(AddressResponseDTO::new)
 				.collect(Collectors.toList());
 	}
+	
+	public static AddressResponseDTO toDto(Address address) {
+		return new AddressResponseDTO(address);
+	}
+	
+	public static Address fromAddressDTO(AddressRequestDTO addressDTO) {
+		Address address = new Address();
+		
+		address.setStreet(addressDTO.street());
+		address.setNumber(addressDTO.number());
+		address.setComplement(addressDTO.complement());
+		address.setDistrict(addressDTO.district());
+		address.setCity(addressDTO.city());
+		address.setState(addressDTO.state());
+		address.setCountry(addressDTO.country());
+		address.setZipCode(addressDTO.zipCode());
+		return address;
+	}
 }
