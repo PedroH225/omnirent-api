aff02396bfa48398fe8d0224c138278cd32a5f0a
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-18 19:27:40 -0300

    feat: define item status enumeration

---
+++ src/main/java/br/com/omnirent/common/enums/ItemStatus.java
@@ -0,0 +1,28 @@
+package br.com.omnirent.common.enums;
+
+public enum ItemStatus {
+    AVAILABLE("Available"),
+    UNAVAILABLE("Unavailable"),
+    RENTED("Rented"),
+    INACTIVE("Inactive"),
+    BLOCKED("Blocked");
+
+    private String itemStatus;
+
+    ItemStatus (String itemStatus){
+        this.itemStatus = itemStatus;
+    }
+    
+	public static ItemStatus fromString(String text) {
+        for (ItemStatus itemStatus : ItemStatus.values()) {
+            if (itemStatus.itemStatus.equalsIgnoreCase(text)) {
+                return itemStatus;
+            }
+        }
+        throw new IllegalArgumentException("Item status enumaration not found: " + text);
+    }
+
+    public String toString(){
+        return itemStatus;
+    }
+}
