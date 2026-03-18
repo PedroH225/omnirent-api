3bedcec4964471975aefa0f6889063bc81786482
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-18 13:51:42 -0300

    feat: define item condition string values

--- src/main/java/br/com/omnirent/common/enums/ItemCondition.java
+++ src/main/java/br/com/omnirent/common/enums/ItemCondition.java
@@ -1,9 +1,20 @@
 package br.com.omnirent.common.enums;
 
 public enum ItemCondition {
-	NEW,
-	LIKE_NEW,
-	GOOD,
-	USED,
-	HEAVILY_USED;
+	NEW("New"),
+	LIKE_NEW("Like new"),
+	GOOD("Good"),
+	USED("Used"),
+	HEAVILY_USED("Heavily used");
+	
+	private String itemCondition;
+	
+	private ItemCondition(String itemCondition) {
+		this.itemCondition = itemCondition;
+	}
+	
+	@Override
+	public String toString() {
+		return this.itemCondition;
+	}
 }
