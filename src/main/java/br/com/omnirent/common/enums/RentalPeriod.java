eb5e60519b3502e784b449a32fcdd7081504cce2
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-23 16:51:29 -0300

    feat: implement rental enums operations

--- src/main/java/br/com/omnirent/common/enums/RentalPeriod.java
+++ src/main/java/br/com/omnirent/common/enums/RentalPeriod.java
@@ -1,8 +1,26 @@
 package br.com.omnirent.common.enums;
 
 public enum RentalPeriod {
-	HOURLY,
-	DAILY,
-	WEEKLY,
-	MONTHLY;
+	HOURLY("Hourly",0.2),
+	DAILY("Daily", 1.0),
+	WEEKLY("Weekly", 5.5),
+	MONTHLY("Monthly", 22);
+	
+	private final String rentalPeriod;
+	
+	private final double multiplier;
+	
+	RentalPeriod(String rentalPeriod, double multiplier) {
+		this.rentalPeriod = rentalPeriod;
+		this.multiplier = multiplier;
+	}
+	
+	public double getMultiplier() {
+		return this.multiplier;
+	}
+	
+	@Override
+	public String toString() {
+		return this.rentalPeriod;
+	}
 }
--- src/main/java/br/com/omnirent/common/enums/RentalStatus.java
+++ src/main/java/br/com/omnirent/common/enums/RentalStatus.java
@@ -1,9 +1,28 @@
 package br.com.omnirent.common.enums;
 
 public enum RentalStatus {
-	RESERVED,
-	ACTIVE,
-	RETURNED,
-	CANCELLED,
-	LATE;
+	RESERVED("Reserved"),
+	ACTIVE("Active"),
+	RETURNED("Returned"),
+	CANCELLED("Cancelled"),
+	LATE("Late");
+	
+	private String rentalStatus;
+	
+	RentalStatus (String rentalStatus){
+        this.rentalStatus = rentalStatus;
+    }
+    
+	public static RentalStatus fromString(String text) {
+        for (RentalStatus rentalStatus : RentalStatus.values()) {
+            if (rentalStatus.rentalStatus.equalsIgnoreCase(text)) {
+                return rentalStatus;
+            }
+        }
+        throw new IllegalArgumentException("Rental status enumaration not found: " + text);
+    }
+
+    public String toString(){
+        return rentalStatus;
+    }
 }
