3b6b258a498294aac8a39b8cf7cea9a5dcbf8973
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-23 17:07:03 -0300

    feat: add chronounit field to rentalPeriod enum

--- src/main/java/br/com/omnirent/common/enums/RentalPeriod.java
+++ src/main/java/br/com/omnirent/common/enums/RentalPeriod.java
@@ -1,22 +1,31 @@
 package br.com.omnirent.common.enums;
 
+import java.time.temporal.ChronoUnit;
+
 public enum RentalPeriod {
-	HOURLY("Hourly",0.2),
-	DAILY("Daily", 1.0),
-	WEEKLY("Weekly", 5.5),
-	MONTHLY("Monthly", 22);
+	HOURLY("Hourly", ChronoUnit.HOURS, 0.2),
+	DAILY("Daily", ChronoUnit.DAYS, 1.0),
+	WEEKLY("Weekly", ChronoUnit.WEEKS, 5.5),
+	MONTHLY("Monthly", ChronoUnit.MONTHS, 22);
 	
 	private final String rentalPeriod;
 	
+	private final ChronoUnit unit;
+	
 	private final double multiplier;
 	
-	RentalPeriod(String rentalPeriod, double multiplier) {
+	RentalPeriod(String rentalPeriod, ChronoUnit unit, double multiplier) {
 		this.rentalPeriod = rentalPeriod;
 		this.multiplier = multiplier;
+		this.unit = unit;
 	}
 	
 	public double getMultiplier() {
 		return this.multiplier;
+	}
+	
+	public ChronoUnit getChronoUnit() {
+		return this.unit;
 	}
 	
 	@Override
