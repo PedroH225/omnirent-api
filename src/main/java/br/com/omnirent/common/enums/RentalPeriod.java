135df269a196e9812b40307245a841f9ec4c1065
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-23 17:09:27 -0300

    refactor: change multiplier type to BigDecimal

--- src/main/java/br/com/omnirent/common/enums/RentalPeriod.java
+++ src/main/java/br/com/omnirent/common/enums/RentalPeriod.java
@@ -1,26 +1,27 @@
 package br.com.omnirent.common.enums;
 
+import java.math.BigDecimal;
 import java.time.temporal.ChronoUnit;
 
 public enum RentalPeriod {
-	HOURLY("Hourly", ChronoUnit.HOURS, 0.2),
-	DAILY("Daily", ChronoUnit.DAYS, 1.0),
-	WEEKLY("Weekly", ChronoUnit.WEEKS, 5.5),
-	MONTHLY("Monthly", ChronoUnit.MONTHS, 22);
+	HOURLY("Hourly", ChronoUnit.HOURS, new BigDecimal("0.2")),
+	DAILY("Daily", ChronoUnit.DAYS, BigDecimal.ONE),
+	WEEKLY("Weekly", ChronoUnit.WEEKS, new BigDecimal("5.5")),
+	MONTHLY("Monthly", ChronoUnit.MONTHS, new BigDecimal("22"));
 	
 	private final String rentalPeriod;
 	
 	private final ChronoUnit unit;
 	
-	private final double multiplier;
+	private final BigDecimal multiplier;
 	
-	RentalPeriod(String rentalPeriod, ChronoUnit unit, double multiplier) {
+	RentalPeriod(String rentalPeriod, ChronoUnit unit, BigDecimal multiplier) {
 		this.rentalPeriod = rentalPeriod;
 		this.multiplier = multiplier;
 		this.unit = unit;
 	}
 	
-	public double getMultiplier() {
+	public BigDecimal getMultiplier() {
 		return this.multiplier;
 	}
 	
