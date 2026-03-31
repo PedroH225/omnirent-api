e7d3f85fd8778f0ccf002f7b8d986cda4686cf6b
PedroH225 <pedrodguimaraes20@gmail.com>
2026-03-31 17:01:43 -0300

    refactor: move rental entities to domain package

--- src/main/java/br/com/omnirent/rental/RentalRepository.java
+++ src/main/java/br/com/omnirent/rental/RentalRepository.java
@@ -3,6 +3,8 @@ package br.com.omnirent.rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.omnirent.rental.domain.Rental;
+
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, String> {
 
