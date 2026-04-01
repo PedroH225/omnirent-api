d59644e1a2053ed0afb0a1fccafc151e34d0440b
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-01 20:17:41 -0300

    feat: implement mark late rentals schedule

--- src/main/java/br/com/omnirent/rental/RentalSchedule.java
+++ src/main/java/br/com/omnirent/rental/RentalSchedule.java
@@ -1,7 +1,15 @@
 package br.com.omnirent.rental;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import br.com.omnirent.common.enums.RentalStatus;
+import br.com.omnirent.rental.domain.Rental;
+import jakarta.transaction.Transactional;
 
 @Component
 public class RentalSchedule {
@@ -9,4 +17,12 @@ public class RentalSchedule {
 	@Autowired
 	public RentalRepository rentalRepository;
 
+	@Transactional
+	@Scheduled(fixedRate = 30000)
+	public void updateLateRentals() {
+		List<Rental> rentals = rentalRepository.
+				findByRentalStatusAndEndDateBefore(RentalStatus.IN_USE, LocalDateTime.now());
+		
+		rentals.forEach(Rental::markLate);
+	}
 }
