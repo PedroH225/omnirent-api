48e6c7ceac01160c4bd6294735dd70f6d980b233
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-02 15:49:29 -0300

    feat: add standard error response body entity

---
+++ src/main/java/br/com/omnirent/exception/common/StandardError.java
@@ -0,0 +1,32 @@
+package br.com.omnirent.exception.common;
+
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StandardError {
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant timestamp;
+	
+	private Integer status;
+	
+	private String error;
+	
+	private String message;
+	
+	private String path;
+
+}
+
+
+
+
+
