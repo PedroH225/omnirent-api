655049a960d3a4252c0d067e9e8c21e61d38440d
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-06 16:55:19 -0300

    refactor: change rental state exception to extend conflict exception

--- src/main/java/br/com/omnirent/exception/domain/IllegalRentalStateException.java
+++ src/main/java/br/com/omnirent/exception/domain/IllegalRentalStateException.java
@@ -1,16 +1,14 @@
 package br.com.omnirent.exception.domain;
 
-import org.springframework.http.HttpStatus;
-
 import br.com.omnirent.common.enums.RentalStatus;
-import br.com.omnirent.exception.common.BusinessException;
+import br.com.omnirent.exception.common.ConflictException;
 
-public class IllegalRentalStateException extends BusinessException {
+public class IllegalRentalStateException extends ConflictException {
 	private static final long serialVersionUID = 1L;
 
 	public IllegalRentalStateException(RentalStatus currentStatus, RentalStatus targetStatus) {
-		super(String.format("Illegal rental state transition: %s to %s", currentStatus, targetStatus),
-				HttpStatus.CONFLICT, "Illegal rental state transition");
+		super(String.format("Illegal rental state transition: %s to %s",
+				currentStatus, targetStatus));
 	}
 
 }
