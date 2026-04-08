ffeed1402dd634b1b11184bcbf43a69206992ece
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-08 16:46:29 -0300

    feat: require item ownership check

--- src/main/java/br/com/omnirent/item/ItemAuthorizationService.java
+++ src/main/java/br/com/omnirent/item/ItemAuthorizationService.java
@@ -2,10 +2,19 @@ package br.com.omnirent.item;
 
 import org.springframework.stereotype.Service;
 
+import br.com.omnirent.exception.common.ForbiddenException;
+import br.com.omnirent.item.domain.Item;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Service
 public class ItemAuthorizationService {
+	
+	private final String DEFAULT_MESSAGE = "You are not allowed to perform this operation.";
 
+	public void requireOwner(Item item, String currentUserId) {
+		if (!item.getOwnerId().equals(currentUserId)) {
+			throw new ForbiddenException(DEFAULT_MESSAGE);
+		}
+	}
 }
