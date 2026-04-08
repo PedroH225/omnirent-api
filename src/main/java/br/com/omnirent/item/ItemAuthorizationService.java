9776cc14c78f2bfa7ac49e838e0f61d413e11d25
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-08 16:37:44 -0300

    feat: item authorization service

---
+++ src/main/java/br/com/omnirent/item/ItemAuthorizationService.java
@@ -0,0 +1,11 @@
+package br.com.omnirent.item;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class ItemAuthorizationService {
+
+}
