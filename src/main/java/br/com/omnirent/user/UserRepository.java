ce5498cd862ed65e64cc75300e712b68b3874766
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-10 16:38:36 -0300

    feat: find user auth metadata

--- src/main/java/br/com/omnirent/user/UserRepository.java
+++ src/main/java/br/com/omnirent/user/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +17,7 @@ public interface UserRepository extends JpaRepository<User, String> {
 	
 	@Query("SELECT u FROM User u WHERE u.email = :email")
 	Optional<User> findExistingUserByEmail(String email);
+	
+	@Query("SELECT u.authMetadata FROM User u WHERE u.id = :id")
+	AuthMetadata findTokenVersionById(@Param("id") String id);
 }
