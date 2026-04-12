932dd493a6532a15d711afc07d6628a22d471398
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-12 00:06:30 -0300

    feat: get subcategory dto by id query

--- src/main/java/br/com/omnirent/category/SubCategoryRepository.java
+++ src/main/java/br/com/omnirent/category/SubCategoryRepository.java
@@ -3,10 +3,18 @@ package br.com.omnirent.category;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory, String> {
 	List<SubCategory> findAllByCategoryName(String categoryName);
+	
+	@Query("""
+			SELECT new br.com.omnirent.category.SubCategoryResDTO(sc.id, sc.name, c.name)
+			FROM SubCategory sc JOIN sc.category c
+			WHERE c.id = :id
+			""")
+	List<SubCategoryResDTO> findSubByCategoryId(String id);
 
 }
