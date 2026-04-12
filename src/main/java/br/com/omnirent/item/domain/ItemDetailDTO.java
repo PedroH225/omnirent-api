eb837ea7c72cf2c23ab18af4384d1bdc6a925522
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-12 01:13:26 -0300

    refactor: item details dto field only constructor

---
+++ src/main/java/br/com/omnirent/item/domain/ItemDetailDTO.java
@@ -0,0 +1,68 @@
+package br.com.omnirent.item.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import br.com.omnirent.address.AddressResponseDTO;
+import br.com.omnirent.category.SubCategoryResDTO;
+import br.com.omnirent.common.enums.ItemCondition;
+import br.com.omnirent.common.enums.ItemStatus;
+import br.com.omnirent.user.domain.UserResponseDTO;
+import lombok.Data;
+
+@Data
+public class ItemDetailDTO {
+	
+	private String id;
+	
+	private String name;
+	
+	private String brand;
+
+	private String model;
+
+	private String description;
+	
+	private BigDecimal basePrice;
+	
+	private String itemCondition;
+	
+	private String itemStatus;
+	
+	private SubCategoryResDTO subCategory;
+	
+	private AddressResponseDTO pickupAddress;
+	
+	private UserResponseDTO owner;
+	
+	private String createdAt;
+	
+	private String updatedAt;
+	
+	@JsonIgnore
+	private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+	public ItemDetailDTO(String id, String name, String brand, String model, String description,
+			BigDecimal basePrice, ItemCondition itemCondition, ItemStatus itemStatus, SubCategoryResDTO subCategory,
+			AddressResponseDTO pickupAddress, UserResponseDTO owner, LocalDateTime createdAt, LocalDateTime updatedAt) {
+		this.id = id;
+		this.name = name;
+		this.brand = brand;
+		this.model = model;
+		this.description = description;
+		this.basePrice = basePrice;
+		this.itemCondition = itemCondition.toString();
+		this.itemStatus = itemStatus.toString();
+		this.subCategory = subCategory;
+		this.pickupAddress = pickupAddress;
+		this.owner = owner;
+		this.createdAt = dtf.format(createdAt);
+		this.updatedAt = dtf.format(updatedAt);
+	}
+
+	
+
+}
