021672fa89cb8eb34cd7a78b3d33b5605e7e31c4
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-11 23:59:28 -0300

    feat: get category dto by id query

--- src/main/java/br/com/omnirent/category/CategoryRepository.java
+++ src/main/java/br/com/omnirent/category/CategoryRepository.java
@@ -1,9 +1,19 @@
 package br.com.omnirent.category;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
 
+	@Query("""
+			SELECT new br.com.omnirent.category.CategoryResponseDTO(c.id, c.name)
+			FROM Category c WHERE c.id = :id
+			""")
+	Optional<CategoryResponseDTO> getCategoryById(String id);
+	
 }
