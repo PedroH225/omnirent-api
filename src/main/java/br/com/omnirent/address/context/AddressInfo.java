17529384b3c46538c0ae5c9a6d7c6bdbbdd924ae
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-15 20:08:00 -0300

    feat: address info class

---
+++ src/main/java/br/com/omnirent/address/context/AddressInfo.java
@@ -0,0 +1,37 @@
+package br.com.omnirent.address.context;
+
+import lombok.Data;
+
+@Data
+public class AddressInfo {
+	
+	private String id;
+
+	private String street;
+
+	private String number;
+
+	private String complement;
+	
+	private String district;
+	
+	private String city;
+
+	private String state;
+
+	private String country;
+
+	private String zipCode;
+
+	public AddressInfo(String street, String number, String complement, String district, String city, String state,
+			String country, String zipCode) {
+		this.street = street;
+		this.number = number;
+		this.complement = complement;
+		this.district = district;
+		this.city = city;
+		this.state = state;
+		this.country = country;
+		this.zipCode = zipCode;
+	}
+}
