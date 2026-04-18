f76de9aa38e5d75094c54978f01f3d96a760401d
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-17 23:54:17 -0300

    feat: user factory helper

---
+++ src/test/java/br/com/omnirent/factory/UserTestFactory.java
@@ -0,0 +1,27 @@
+package br.com.omnirent.factory;
+
+import java.time.LocalDate;
+
+import br.com.omnirent.user.domain.User;
+import br.com.omnirent.utils.Sequence;
+
+public final class UserTestFactory {
+
+    private UserTestFactory() {}
+
+    public static User owner() {
+    	String owner = Sequence.nextString("owner-");
+        return new User(
+        	owner, owner, owner + "@email.com",
+        	owner, LocalDate.now(), 1, 1
+        );
+    }
+    
+    public static User user() {
+    	String owner = Sequence.nextString("user-");
+        return new User(
+        	owner, owner, owner + "@email.com",
+        	owner, LocalDate.now(), 1, 1
+        );
+    }
+}
