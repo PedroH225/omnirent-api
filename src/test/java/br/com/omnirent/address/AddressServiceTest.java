160d9fa31eab8dbe9d25ba7eebb4f58205565c5b
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-18 19:14:04 -0300

    test: update address

--- src/test/java/br/com/omnirent/address/AddressServiceTest.java
+++ src/test/java/br/com/omnirent/address/AddressServiceTest.java
@@ -1,13 +1,17 @@
 package br.com.omnirent.address;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import java.sql.Savepoint;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +23,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import br.com.omnirent.address.domain.Address;
 import br.com.omnirent.address.dto.AddressRequestDTO;
 import br.com.omnirent.address.dto.AddressResponseDTO;
+import br.com.omnirent.exception.domain.UserNotFoundException;
 import br.com.omnirent.factory.AddressTestFactory;
 import br.com.omnirent.factory.UserTestFactory;
 import br.com.omnirent.user.UserService;
@@ -29,24 +34,24 @@ public class AddressServiceTest {
 
 	@InjectMocks
 	private AddressService addressService;
-	
+
 	@Mock
 	private AddressRepository addressRepository;
-	
+
 	@Mock
-    private UserService userService;
-	
+	private UserService userService;
+
 	@Mock
-    private AddressMapper mapper;
-	
+	private AddressMapper mapper;
+
 	private User user;
 
 	private User user2;
-	
+
 	private Address userAddress;
-	
+
 	private Address userAddress2;
-	
+
 	@BeforeEach
 	void setUp() {
 		user = UserTestFactory.persistedUser();
@@ -55,49 +60,81 @@ public class AddressServiceTest {
 
 		user2 = UserTestFactory.persistedUser();
 	}
-	
+
 	@Test
 	void shouldReturnUserAddresses() {
-	    String userId = user.getId();
+		String userId = user.getId();
 
-	    AddressResponseDTO dto1 = AddressTestFactory.toAddressDto(userAddress);
-	    
-	    AddressResponseDTO dto2 = AddressTestFactory.toAddressDto(userAddress2);
+		AddressResponseDTO dto1 = AddressTestFactory.toAddressDto(userAddress);
 
-	    List<AddressResponseDTO> expected = List.of(dto1, dto2);
+		AddressResponseDTO dto2 = AddressTestFactory.toAddressDto(userAddress2);
 
-	    when(addressRepository.findAddressByUser(userId))
-	            .thenReturn(expected);
+		List<AddressResponseDTO> expected = List.of(dto1, dto2);
 
-	    List<AddressResponseDTO> result =
-	            addressService.getUserAddresses(userId);
+		when(addressRepository.findAddressByUser(userId)).thenReturn(expected);
 
-	    assertThat(result).isEqualTo(expected);
+		List<AddressResponseDTO> result = addressService.getUserAddresses(userId);
 
-	    verify(addressRepository).findAddressByUser(userId);
-	    verifyNoMoreInteractions(addressRepository);
+		assertThat(result).isEqualTo(expected);
+
+		verify(addressRepository).findAddressByUser(userId);
+		verifyNoMoreInteractions(addressRepository);
 	}
-	
+
 	@Test
 	void shouldAddAddress() {
-	    AddressRequestDTO addressDTO = AddressTestFactory.toRequestDTO(userAddress);
-	    String userId = user.getId();
+		AddressRequestDTO addressDTO = AddressTestFactory.toRequestDTO(userAddress);
+		String userId = user.getId();
 
-	    AddressResponseDTO responseDTO = AddressTestFactory.toAddressDto(userAddress);
+		AddressResponseDTO responseDTO = AddressTestFactory.toAddressDto(userAddress);
 
-	    doNothing().when(userService).requireExistence(userId);
-	    when(mapper.fromAddressDTO(addressDTO, userId)).thenReturn(userAddress);
-	    when(addressRepository.save(userAddress)).thenReturn(userAddress);
-	    when(mapper.toDto(userAddress)).thenReturn(responseDTO);
+		doNothing().when(userService).requireExistence(userId);
+		when(mapper.fromAddressDTO(addressDTO, userId)).thenReturn(userAddress);
+		when(addressRepository.save(userAddress)).thenReturn(userAddress);
+		when(mapper.toDto(userAddress)).thenReturn(responseDTO);
 
-	    AddressResponseDTO result = addressService.addAddress(addressDTO, userId);
+		AddressResponseDTO result = addressService.addAddress(addressDTO, userId);
 
-	    assertThat(result).isEqualTo(responseDTO);
+		assertThat(result).isEqualTo(responseDTO);
 
-	    verify(userService).requireExistence(userId);
-	    verify(mapper).fromAddressDTO(addressDTO, userId);
-	    verify(addressRepository).save(userAddress);
-	    verify(mapper).toDto(userAddress);
-	    verifyNoMoreInteractions(userService, mapper, addressRepository);
+		verify(userService).requireExistence(userId);
+		verify(mapper).fromAddressDTO(addressDTO, userId);
+		verify(addressRepository).save(userAddress);
+		verify(mapper).toDto(userAddress);
+		verifyNoMoreInteractions(userService, mapper, addressRepository);
 	}
+
+	@Test
+	void shouldThrowWhenInvalidUser() {
+		AddressRequestDTO addressDTO = AddressTestFactory.toRequestDTO(userAddress);
+		String userId = "invalid-id";
+
+		doThrow(new UserNotFoundException()).when(userService).requireExistence(userId);
+
+		assertThatThrownBy(() -> addressService.addAddress(addressDTO, userId))
+				.isInstanceOf(UserNotFoundException.class);
+
+		verify(userService).requireExistence(userId);
+		verifyNoMoreInteractions(addressRepository, mapper);
+	}
+
+	@Test
+	void shouldUpdateAddress() {
+		AddressRequestDTO addressDTO = AddressTestFactory.toRequestDTO(userAddress);
+		AddressResponseDTO responseDTO = AddressTestFactory.toAddressDto(userAddress);
+
+		when(addressRepository.findById(addressDTO.id())).thenReturn(Optional.of(userAddress));
+		when(addressRepository.save(userAddress)).thenReturn(userAddress);
+		when(mapper.toDto(userAddress)).thenReturn(responseDTO);
+
+		AddressResponseDTO result = addressService.updateAddress(addressDTO);
+
+		assertThat(result).isEqualTo(responseDTO);
+
+		verify(addressRepository).findById(addressDTO.id());
+		verify(addressRepository).save(userAddress);
+		verify(mapper).toDto(userAddress);
+		verifyNoMoreInteractions(addressRepository, mapper);
+	}
+
 }
