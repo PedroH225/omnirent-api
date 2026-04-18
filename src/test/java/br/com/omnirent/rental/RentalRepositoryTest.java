91167c724b433c8d6352319c1cbe3eeb781371e2
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-18 01:01:13 -0300

    test: find rental details

--- src/test/java/br/com/omnirent/rental/RentalRepositoryTest.java
+++ src/test/java/br/com/omnirent/rental/RentalRepositoryTest.java
@@ -2,6 +2,9 @@ package br.com.omnirent.rental;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,9 @@ import org.springframework.context.annotation.Import;
 
 import br.com.omnirent.address.AddressRepository;
 import br.com.omnirent.address.domain.Address;
+import br.com.omnirent.address.domain.AddressData;
+import br.com.omnirent.address.domain.AddressSnapshot;
+import br.com.omnirent.address.dto.AddressSnapshotDTO;
 import br.com.omnirent.category.CategoryRepository;
 import br.com.omnirent.category.SubCategoryRepository;
 import br.com.omnirent.category.domain.Category;
@@ -28,9 +34,14 @@ import br.com.omnirent.factory.UserTestFactory;
 import br.com.omnirent.integration.IntegrationTest;
 import br.com.omnirent.item.ItemRepository;
 import br.com.omnirent.item.domain.Item;
+import br.com.omnirent.item.domain.ItemData;
+import br.com.omnirent.item.domain.ItemSnapshot;
+import br.com.omnirent.item.dto.ItemSnapshotDTO;
 import br.com.omnirent.rental.domain.Rental;
+import br.com.omnirent.rental.dto.RentalDetailDTO;
 import br.com.omnirent.user.UserRepository;
 import br.com.omnirent.user.domain.User;
+import br.com.omnirent.user.dto.UserResponseDTO;
 import jakarta.transaction.Transactional;
 
 @Transactional
@@ -70,10 +81,13 @@ public class RentalRepositoryTest extends IntegrationTest {
 	
 	private Rental rental;
 	
+	private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
+	
 	@BeforeEach
 	void setUp() {
 		owner = userRepository.save(UserTestFactory.owner());
 		renter = userRepository.save(UserTestFactory.user());
+		
 		ownerAddress = addressRepository.save(AddressTestFactory.forUser(owner));
         tools = categoryRepository.save(CategoryTestFactory.create("Tools"));
         drill = subRepository.save(SubCategoryTestFactory.create("Drill", tools));
@@ -89,8 +103,52 @@ public class RentalRepositoryTest extends IntegrationTest {
 	}
 	
 	@Test
-	void test() {
-		assertThat(rental).isNotNull();
-		System.out.println("Rentalid:" + rental.getId());
+	void shouldFindRentalDetail() {
+	    Optional<RentalDetailDTO> result = rentalRepository.findRentalDetail(rental.getId());
+
+	    assertThat(result).isPresent();
+
+	    RentalDetailDTO dto = result.get();
+
+	    ItemSnapshotDTO itemSnpDto = dto.getItemSnapshot();
+	    AddressSnapshotDTO adrsSnpDto = dto.getAddressSnapshot();
+	    UserResponseDTO ownerDTO = dto.getOwner();
+	    UserResponseDTO renterDto = dto.getRenter();
+
+	    AddressSnapshot adrsSnp = rental.getAddressSnapshot();
+	    AddressData adrsData = adrsSnp.getAddressData();
+	    ItemSnapshot itemSnp = rental.getItemSnapshot();
+	    ItemData itemData = itemSnp.getItemData();
+
+	    assertThat(dto.getId()).isEqualTo(rental.getId());
+	    assertThat(dto.getStartDate()).isEqualTo(dtf.format(rental.getStartDate()));
+	    assertThat(dto.getEndDate()).isEqualTo(dtf.format(rental.getEndDate()));
+	    assertThat(dto.getFinalPrice()).isEqualByComparingTo(rental.getFinalPrice());
+	    assertThat(dto.getRentalStatus()).isEqualTo(rental.getRentalStatus().toString());
+	    assertThat(dto.getRentalPeriod()).isEqualTo(rental.getRentalPeriod().toString());
+
+	    assertThat(renterDto.getId()).isEqualTo(renter.getId());
+	    assertThat(renterDto.getUsername()).isEqualTo(renter.getDisplayUsername());
+
+	    assertThat(ownerDTO.getId()).isEqualTo(owner.getId());
+	    assertThat(ownerDTO.getUsername()).isEqualTo(owner.getDisplayUsername());
+
+	    assertThat(itemSnpDto.getId()).isEqualTo(itemSnp.getId());
+	    assertThat(itemSnpDto.getName()).isEqualTo(itemSnp.getName());
+	    assertThat(itemSnpDto.getBrand()).isEqualTo(itemData.getBrand());
+	    assertThat(itemSnpDto.getModel()).isEqualTo(itemData.getModel());
+	    assertThat(itemSnpDto.getBasePrice()).isEqualByComparingTo(itemData.getBasePrice());
+	    assertThat(itemSnpDto.getItemCondition()).isEqualTo(itemData.getItemCondition().toString());
+	    assertThat(itemSnpDto.getDescription()).isEqualTo(itemData.getDescription());
+
+	    assertThat(adrsSnpDto.getId()).isEqualTo(adrsSnp.getId());
+	    assertThat(adrsSnpDto.getStreet()).isEqualTo(adrsData.getStreet());
+	    assertThat(adrsSnpDto.getNumber()).isEqualTo(adrsData.getNumber());
+	    assertThat(adrsSnpDto.getComplement()).isEqualTo(adrsData.getComplement());
+	    assertThat(adrsSnpDto.getDistrict()).isEqualTo(adrsData.getDistrict());
+	    assertThat(adrsSnpDto.getCity()).isEqualTo(adrsData.getCity());
+	    assertThat(adrsSnpDto.getState()).isEqualTo(adrsData.getState());
+	    assertThat(adrsSnpDto.getCountry()).isEqualTo(adrsData.getCountry());
+	    assertThat(adrsSnpDto.getZipCode()).isEqualTo(adrsData.getZipCode());
 	}
 }
