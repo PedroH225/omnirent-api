b230c38aa19d37f2a6159e319c4d4615d6904a22
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-18 17:37:59 -0300

    feat: persisted user and address generation

--- src/test/java/br/com/omnirent/factory/AddressTestFactory.java
+++ src/test/java/br/com/omnirent/factory/AddressTestFactory.java
@@ -1,8 +1,12 @@
 package br.com.omnirent.factory;
 
+import java.time.LocalDateTime;
+
 import br.com.omnirent.address.domain.Address;
 import br.com.omnirent.address.domain.AddressData;
+import br.com.omnirent.address.dto.AddressResponseDTO;
 import br.com.omnirent.user.domain.User;
+import br.com.omnirent.utils.Sequence;
 
 public final class AddressTestFactory {
 
@@ -22,4 +26,29 @@ public final class AddressTestFactory {
         address.setUserId(owner.getId());
         return address;
     }
+    
+    
+    public static Address forPersistedUser(User user) {
+    	Address address = forUser(user);
+    	address.setId(Sequence.nextString("addressId"));
+    	address.setCreatedAt(LocalDateTime.now());
+    	address.setUpdatedAt(LocalDateTime.now());
+    	return address;
+    }
+    
+    public static AddressResponseDTO toAddressDto(Address address) {
+		return new AddressResponseDTO(
+			    address.getId(),
+			    address.getAddressData().getStreet(),
+			    address.getAddressData().getNumber(),
+			    address.getAddressData().getComplement(),
+			    address.getAddressData().getDistrict(),
+			    address.getAddressData().getCity(),
+			    address.getAddressData().getState(),
+			    address.getAddressData().getCountry(),
+			    address.getAddressData().getZipCode(),
+			    address.getCreatedAt(),
+			    address.getUpdatedAt()
+			);
+	}
 }
--- src/test/java/br/com/omnirent/factory/UserTestFactory.java
+++ src/test/java/br/com/omnirent/factory/UserTestFactory.java
@@ -1,6 +1,7 @@
 package br.com.omnirent.factory;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import br.com.omnirent.user.domain.User;
 import br.com.omnirent.utils.Sequence;
@@ -23,5 +24,13 @@ public final class UserTestFactory {
         	owner, owner, owner + "@email.com",
         	owner, LocalDate.now(), 1, 1
         );
+    }
+    
+    public static User persistedUser() {
+    	User user = user();
+    	user.setCreatedAt(LocalDateTime.now());
+    	user.setUpdatedAt(LocalDateTime.now());
+    	user.setId(Sequence.nextString("userId"));
+    	return user;
     }
 }
