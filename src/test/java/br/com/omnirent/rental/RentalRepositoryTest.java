cd57d8e64358217b85ff34d784a5b98fccbc9842
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-18 01:50:39 -0300

    test: mark late rentals

--- src/test/java/br/com/omnirent/rental/RentalRepositoryTest.java
+++ src/test/java/br/com/omnirent/rental/RentalRepositoryTest.java
@@ -300,5 +300,15 @@ public class RentalRepositoryTest extends IntegrationTest {
 	    assertThat(result).isEmpty();
 	}
 
+	@Test
+	void shouldMarkLateRentals() {
+		assertThat(rental2.getRentalStatus()).isEqualTo(RentalStatus.IN_USE);
+		
+		rentalRepository.markLate(RentalStatus.LATE, RentalStatus.IN_USE);
+		Optional<RentalDisplayDTO> lateRental = rentalRepository.findRentalDisplayDTO(rental2.getId());
+	
+		assertThat(lateRental).isPresent();
+		assertThat(lateRental.get().getRentalStatus()).isEqualTo(RentalStatus.LATE.toString());
+	}
 }
 
