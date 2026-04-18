71f524f4a5dfb09b1084691e1c8beb8fedf8f8d7
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-18 17:39:16 -0300

    test: mocked AddressService setup

--- src/test/java/br/com/omnirent/address/AddressServiceTest.java
+++ src/test/java/br/com/omnirent/address/AddressServiceTest.java
@@ -1,42 +1,56 @@
 package br.com.omnirent.address;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.omnirent.address.domain.Address;
+import br.com.omnirent.address.dto.AddressResponseDTO;
 import br.com.omnirent.factory.AddressTestFactory;
 import br.com.omnirent.factory.UserTestFactory;
-import br.com.omnirent.integration.SpringIntegrationTest;
-import br.com.omnirent.user.UserRepository;
+import br.com.omnirent.user.UserService;
 import br.com.omnirent.user.domain.User;
-import jakarta.transaction.Transactional;
 
-@Transactional
-public class AddressServiceTest extends SpringIntegrationTest {
+@ExtendWith(MockitoExtension.class)
+public class AddressServiceTest {
 
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private AddressRepository addressRepository;
-	
-	@Autowired
+	@InjectMocks
 	private AddressService addressService;
 	
+	@Mock
+	private AddressRepository addressRepository;
+	
+	@Mock
+    private UserService userService;
+	
+	@Mock
+    private AddressMapper mapper;
+	
 	private User user;
+
+	private User user2;
 	
 	private Address userAddress;
 	
+	private Address userAddress2;
+	
 	@BeforeEach
 	void setUp() {
-		user = userRepository.save(UserTestFactory.user());
-		userAddress = addressRepository.save(AddressTestFactory.forUser(user));
-	}
- 	
-	@Test
-	void test() {
-		System.out.println(user.getId());
-		System.out.println(userAddress.getId());
+		user = UserTestFactory.persistedUser();
+		userAddress = AddressTestFactory.forPersistedUser(user);
+		userAddress2 = AddressTestFactory.forPersistedUser(user);
+
+		user2 = UserTestFactory.persistedUser();
 	}
 }
