5b21b2d0cc9e6f627c8b242d39cb8c570f920ad8
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-22 19:40:05 -0300

    test: get item rented context

--- src/test/java/br/com/omnirent/item/ItemServiceTest.java
+++ src/test/java/br/com/omnirent/item/ItemServiceTest.java
@@ -3,7 +3,6 @@ package br.com.omnirent.item;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
@@ -17,7 +16,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.omnirent.address.AddressService;
+import br.com.omnirent.address.context.AddressInfo;
 import br.com.omnirent.address.domain.Address;
+import br.com.omnirent.address.dto.AddressResponseDTO;
 import br.com.omnirent.category.CategoryService;
 import br.com.omnirent.category.domain.Category;
 import br.com.omnirent.category.domain.SubCategory;
@@ -28,11 +29,14 @@ import br.com.omnirent.factory.CategoryTestFactory;
 import br.com.omnirent.factory.ItemTestFactory;
 import br.com.omnirent.factory.SubCategoryTestFactory;
 import br.com.omnirent.factory.UserTestFactory;
+import br.com.omnirent.item.context.ItemInfo;
+import br.com.omnirent.item.context.ItemRentedContext;
 import br.com.omnirent.item.domain.Item;
 import br.com.omnirent.item.dto.ItemDetailDTO;
 import br.com.omnirent.security.CurrentUserProvider;
 import br.com.omnirent.user.UserService;
 import br.com.omnirent.user.domain.User;
+import br.com.omnirent.user.dto.UserResponseDTO;
 
 @ExtendWith(MockitoExtension.class)
 public class ItemServiceTest {
@@ -110,6 +114,21 @@ public class ItemServiceTest {
 			.isInstanceOf(ItemNotFoundException.class);
 				
 		verify(itemRepository).findItemDetailDTO(invalidId);
+		verifyNoMoreInteractions(itemRepository);
+	}
+	
+	@Test
+	void shouldGetItemRentedContextById() {
+		String itemId = item.getId();
+		ItemRentedContext context = ItemTestFactory.toItemRentedContext(item, ownerAddress, owner);
+	
+		when(itemRepository.getItemRentedContext(itemId)).thenReturn(Optional.of(context));
+		
+		ItemRentedContext result = itemService.getItemRentedContext(itemId);
+		
+		assertThat(result).isEqualTo(context);
+		
+		verify(itemRepository).getItemRentedContext(itemId);
 		verifyNoMoreInteractions(itemRepository);
 	}
 }
