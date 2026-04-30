46ecdf35db22f494d2207d7307243c23c09bfe90
PedroH225 <pedrodguimaraes20@gmail.com>
2026-04-30 15:50:47 -0300

    refactor: generate rental from add rent context

--- src/test/java/br/com/omnirent/factory/RentalTestFactory.java
+++ src/test/java/br/com/omnirent/factory/RentalTestFactory.java
@@ -3,14 +3,21 @@ package br.com.omnirent.factory;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import br.com.omnirent.address.context.AddressInfo;
 import br.com.omnirent.address.domain.Address;
 import br.com.omnirent.address.domain.AddressSnapshot;
+import br.com.omnirent.address.dto.AddressSnapshotDTO;
 import br.com.omnirent.common.enums.RentalPeriod;
 import br.com.omnirent.common.enums.RentalStatus;
+import br.com.omnirent.item.context.ItemInfo;
+import br.com.omnirent.item.context.ItemRentedContext;
 import br.com.omnirent.item.domain.Item;
 import br.com.omnirent.item.domain.ItemSnapshot;
+import br.com.omnirent.item.dto.ItemSnapshotDTO;
 import br.com.omnirent.rental.domain.Rental;
+import br.com.omnirent.rental.dto.RentalCreatedDTO;
 import br.com.omnirent.rental.dto.RentalDisplayDTO;
+import br.com.omnirent.rental.dto.RentalRequestDTO;
 import br.com.omnirent.user.domain.User;
 import br.com.omnirent.utils.Sequence;
 
@@ -44,6 +51,29 @@ public final class RentalTestFactory {
 		
 		rental.setRenter(renter);
 		rental.setRenterId(renter.getId());
+		
+		return rental;
+	}
+	
+	public static Rental create(User renter, String renterId, ItemRentedContext context,
+		    RentalPeriod rentalPeriod, RentalStatus rentalStatus,
+		    BigDecimal finalPrice) {
+		Rental rental = new Rental();
+		AddressInfo addressInfo = context.getAddressInfo();
+		ItemInfo item = context.getItemInfo();
+		
+		rental.setOwnerId(context.getOwnerId());
+		rental.assignRenter(renter, renterId);
+		
+		rental.setRentalPeriod(rentalPeriod);
+		
+		rental.setRentalStatus(rentalStatus);
+		
+		rental.setFinalPrice(finalPrice);
+		
+		rental.setAddressSnapshot(AddressTestFactory.toSnapshot(addressInfo, rental));
+		
+		rental.setItemSnapshot(ItemTestFactory.toSnapshot(item, rental));
 		
 		return rental;
 	}
