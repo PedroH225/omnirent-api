276e29a9212a64a4764afcb813a063390ea04dfe
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-13 19:45:00 -0300

    refactor: remove item status string

--- src/main/java/br/com/omnirent/common/enums/ItemStatus.java
+++ src/main/java/br/com/omnirent/common/enums/ItemStatus.java
@@ -1,28 +1,11 @@
 package br.com.omnirent.common.enums;
 
-import br.com.omnirent.exception.domain.IllegalEnumerationException;
-
 public enum ItemStatus {
-    AVAILABLE("Available"),
-    UNAVAILABLE("Unavailable"),
-    BLOCKED("Blocked");
+	AVAILABLE,
+	UNAVAILABLE,
+	BLOCKED;
 
-    private String itemStatus;
-
-    ItemStatus (String itemStatus){
-        this.itemStatus = itemStatus;
-    }
-    
-	public static ItemStatus fromString(String text) {
-        for (ItemStatus itemStatus : ItemStatus.values()) {
-            if (itemStatus.itemStatus.equalsIgnoreCase(text)) {
-                return itemStatus;
-            }
-        }
-        throw new IllegalEnumerationException(ItemStatus.class, text);
-    }
-
-    public String toString(){
-        return itemStatus;
-    }
+	public String getMessageKey() {
+		return "item.status." + name();
+	}
 }
