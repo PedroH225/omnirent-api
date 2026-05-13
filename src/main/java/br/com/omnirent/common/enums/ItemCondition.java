7a87e22eb58a97e354ec99cacc7fa31099771425
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-13 19:44:34 -0300

    refactor: remove item condition string

--- src/main/java/br/com/omnirent/common/enums/ItemCondition.java
+++ src/main/java/br/com/omnirent/common/enums/ItemCondition.java
@@ -1,31 +1,13 @@
 package br.com.omnirent.common.enums;
 
-import br.com.omnirent.exception.domain.IllegalEnumerationException;
-
 public enum ItemCondition {
-	NEW("New"),
-	LIKE_NEW("Like new"),
-	GOOD("Good"),
-	USED("Used"),
-	HEAVILY_USED("Heavily used");
+	NEW,
+	LIKE_NEW,
+	GOOD,
+	USED,
+	HEAVILY_USED;
 	
-	private String itemCondition;
-	
-	private ItemCondition(String itemCondition) {
-		this.itemCondition = itemCondition;
-	}
-	
-	public static ItemCondition fromString(String text) {
-        for (ItemCondition itemCondition : ItemCondition.values()) {
-            if (itemCondition.itemCondition.equalsIgnoreCase(text)) {
-                return itemCondition;
-            }
-        }
-        throw new IllegalEnumerationException(ItemCondition.class, text);
-    }
-	
-	@Override
-	public String toString() {
-		return this.itemCondition;
+	public String getMessageKey() {
+		return "item.condition." + name();
 	}
 }
