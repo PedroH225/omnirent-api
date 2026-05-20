2868222dd0f1061d9032a59b37bef3a6a96359f1
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-20 19:50:45 -0300

    refactor: handle exception localization

--- src/main/java/br/com/omnirent/exception/common/GlobalExceptionHandler.java
+++ src/main/java/br/com/omnirent/exception/common/GlobalExceptionHandler.java
@@ -2,21 +2,33 @@ package br.com.omnirent.exception.common;
 
 import java.time.Instant;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import br.com.omnirent.config.i18n.MessageService;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	
+	@Autowired
+	private MessageService messageService;
+			
 	@ExceptionHandler(exception = BusinessException.class)
 	public ResponseEntity<StandardError> handleException(BusinessException e, HttpServletRequest request) {
-        StandardError err = new StandardError
-        		(Instant.now(), e.getHttpStatus().value(), e.getError(),
-        				e.getMessage(), request.getRequestURI());
+	    String localizedError = messageService.get(e.getErrorKey(), e.getError());
+	    String localizedMessage = messageService.get(e.getMessageKey(), e.getMessage());
+
+	    StandardError err = new StandardError(
+	            Instant.now(),
+	            e.getHttpStatus().value(),
+	            localizedError,
+	            localizedMessage,
+	            request.getRequestURI()
+	    );
         
         return ResponseEntity.status(e.getHttpStatus()).body(err);
 	}
