1972999709639c9ca73a5bbf0107adc9b670d5fc
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-21 15:49:04 -0300

    refactor: rental error type enum

---
+++ src/main/java/br/com/omnirent/exception/domain/RentalErrorType.java
@@ -0,0 +1,33 @@
+package br.com.omnirent.exception.domain;
+
+import org.springframework.http.HttpStatus;
+
+import br.com.omnirent.exception.common.AppErrorType;
+import lombok.Getter;
+
+@Getter
+public enum RentalErrorType implements AppErrorType {
+
+    NOT_FOUND("NOT_FOUND", "RENTAL_NOT_FOUND", "not_found", HttpStatus.NOT_FOUND);
+    	
+    private String errorType;
+	
+	private String errorCode;
+	
+	private String messageKey;
+	
+	private HttpStatus httpCode;
+	
+	RentalErrorType(String errorType, String errorCode, String messageKey,
+			HttpStatus httpCode) {
+		this.errorType = errorType;
+		this.errorCode = errorCode;
+		this.messageKey = messageKey;
+		this.httpCode = httpCode;
+	}
+	
+	@Override
+	public String getMessageKey() {
+		return "error.rental." + this.messageKey;
+	}
+}
