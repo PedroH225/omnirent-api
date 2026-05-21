c551223361bc93bc42ef5c043a7459778208882d
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-21 01:16:07 -0300

    refactor: user error type enum

---
+++ src/main/java/br/com/omnirent/exception/domain/UserErrorType.java
@@ -0,0 +1,33 @@
+package br.com.omnirent.exception.domain;
+
+import org.springframework.http.HttpStatus;
+
+import br.com.omnirent.exception.common.AppErrorType;
+import lombok.Getter;
+
+@Getter
+public enum UserErrorType implements AppErrorType {
+
+    NOT_FOUND("NOT_FOUND", "USER_NOT_FOUND", "not_found", HttpStatus.NOT_FOUND);
+    	
+    private String errorType;
+	
+	private String errorCode;
+	
+	private String messageKey;
+	
+	private HttpStatus httpCode;
+	
+	UserErrorType(String errorType, String errorCode, String messageKey,
+			HttpStatus httpCode) {
+		this.errorType = errorType;
+		this.errorCode = errorCode;
+		this.messageKey = messageKey;
+		this.httpCode = httpCode;
+	}
+	
+	@Override
+	public String getMessageKey() {
+		return "error.user." + this.messageKey;
+	}
+}
