eb4e8c1a3e5300b4eef43e81f46bd0902cc27bc4
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-21 01:43:09 -0300

    refactor: blocked item method throws api exception

--- src/main/java/br/com/omnirent/item/ItemAuthorizationService.java
+++ src/main/java/br/com/omnirent/item/ItemAuthorizationService.java
@@ -3,7 +3,9 @@ package br.com.omnirent.item;
 import org.springframework.stereotype.Service;
 
 import br.com.omnirent.common.enums.ItemStatus;
+import br.com.omnirent.exception.common.ApiException;
 import br.com.omnirent.exception.common.ForbiddenException;
+import br.com.omnirent.exception.domain.ItemErrorType;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -11,8 +13,6 @@ import lombok.AllArgsConstructor;
 public class ItemAuthorizationService {
 	
 	private final String DEFAULT_MESSAGE = "You are not allowed to perform this operation.";
-
-	private final String BLOCKED_MESSAGE = "Item is blocked by the system.";
 
 	public void requireOwner(String ownerId, String currentUserId) {
 		if (!ownerId.equals(currentUserId)) {
@@ -22,7 +22,7 @@ public class ItemAuthorizationService {
 	
 	public void requireNotBlocked(ItemStatus currentStatus) {
 		if (currentStatus == ItemStatus.BLOCKED) {
-			throw new ForbiddenException(BLOCKED_MESSAGE);
+			throw new ApiException(ItemErrorType.BLOCKED);
 		}
 	}
 }
