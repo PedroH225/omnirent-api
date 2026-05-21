a43ae09e7eac746f11d1acb06d23867997687bee
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-21 20:49:44 -0300

    refactor: common error type enum

---
+++ src/main/java/br/com/omnirent/exception/domain/CommonErrorType.java
@@ -0,0 +1,33 @@
+package br.com.omnirent.exception.domain;
+
+import org.springframework.http.HttpStatus;
+
+import br.com.omnirent.exception.common.AppErrorType;
+import lombok.Getter;
+
+@Getter
+public enum CommonErrorType implements AppErrorType {
+
+	INTERNAL_ERROR("INTERNAL_SERVER_ERROR", "INTERNAL_SERVER_ERROR", "internal_server_error", HttpStatus.INTERNAL_SERVER_ERROR);
+	
+    private String errorType;
+	
+	private String errorCode;
+	
+	private String messageKey;
+	
+	private HttpStatus httpCode;
+	
+	CommonErrorType(String errorType, String errorCode, String messageKey,
+			HttpStatus httpCode) {
+		this.errorType = errorType;
+		this.errorCode = errorCode;
+		this.messageKey = messageKey;
+		this.httpCode = httpCode;
+	}
+	
+	@Override
+	public String getMessageKey() {
+		return "error.common." + this.messageKey;
+	}
+}
