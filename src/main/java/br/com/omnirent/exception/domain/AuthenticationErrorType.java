f1d521240f6156d72e13a1d88e0566f08b4ce1e9
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-22 17:12:45 -0300

    refactor: invalid token enum

--- src/main/java/br/com/omnirent/exception/domain/AuthenticationErrorType.java
+++ src/main/java/br/com/omnirent/exception/domain/AuthenticationErrorType.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 @Getter
 public enum AuthenticationErrorType implements AppErrorType {
 
+    INVALID_TOKEN("UNAUTHORIZED", "INVALID_TOKEN", "authentication_required", HttpStatus.UNAUTHORIZED),
     AUTHENTICATION_REQUIRED("UNAUTHORIZED", "AUTHENTICATION_REQUIRED", "authentication_required", HttpStatus.UNAUTHORIZED),
     INVALID_CREDENTIALS("UNAUTHORIZED", "INVALID_CREDENTIALS", "invalid_credentials", HttpStatus.UNAUTHORIZED);
     	
