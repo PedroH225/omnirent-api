86a0eb4eba58da08d56a17a80d870d10caffb728
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-21 21:26:42 -0300

    feat: illegal enum exception handler

--- src/main/java/br/com/omnirent/exception/common/GlobalExceptionHandler.java
+++ src/main/java/br/com/omnirent/exception/common/GlobalExceptionHandler.java
@@ -3,16 +3,15 @@ package br.com.omnirent.exception.common;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 import br.com.omnirent.config.i18n.MessageService;
 import br.com.omnirent.exception.domain.CommonErrorType;
 import jakarta.servlet.http.HttpServletRequest;
+import tools.jackson.databind.exc.InvalidFormatException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -39,5 +38,21 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ApiErrorResponse> handleGeneric(Exception e, HttpServletRequest request) {
 	    return handleException(new ApiException(CommonErrorType.INTERNAL_ERROR), request);
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ApiErrorResponse> handleNotReadable(
+	        HttpMessageNotReadableException e,
+	        HttpServletRequest request
+	) {
+
+		Throwable cause = e.getCause();
+		if (cause instanceof InvalidFormatException ex &&
+	        ex.getTargetType().isEnum()) {
+	        return handleException(
+	            new ApiException(CommonErrorType.ILLEGAL_ENUMERATION, ex.getValue()), request
+	        );
+	    }
+	    return handleGeneric(e, request);
 	}
 }
