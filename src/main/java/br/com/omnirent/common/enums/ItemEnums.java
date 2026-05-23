551e0b0c8560ca0e056dee830ba14e46be148d9d
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-22 21:54:36 -0300

    refactor: item enum using enum option

--- src/main/java/br/com/omnirent/common/enums/ItemEnums.java
+++ src/main/java/br/com/omnirent/common/enums/ItemEnums.java
@@ -1,23 +1,15 @@
 package br.com.omnirent.common.enums;
 
-import java.util.Arrays;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-public final class ItemEnums {
-	
-	private List<String> itemStatuses;
-	
-	private List<String> itemConditions;
+@AllArgsConstructor
+public class ItemEnums {
 
-	public ItemEnums() {
-		this.itemStatuses = Arrays.stream(ItemStatus.values())
-				.map(e -> e.toString())
-				.toList();
-		this.itemConditions = Arrays.stream(ItemCondition.values())
-				.map(e -> e.toString())
-				.toList();
-	}
+	List<EnumOption> itemConditions;
+	
+	List<EnumOption> itemStatuses;
 }
