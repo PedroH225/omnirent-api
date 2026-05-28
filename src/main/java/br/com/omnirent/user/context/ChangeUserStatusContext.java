14e202dffd06e440c1acef35b51b10e6ee9a881f
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-28 20:41:16 -0300

    refactor: change user status context

---
+++ src/main/java/br/com/omnirent/user/context/ChangeUserStatusContext.java
@@ -0,0 +1,8 @@
+package br.com.omnirent.user.context;
+
+import br.com.omnirent.common.enums.UserStatus;
+
+public record ChangeUserStatusContext(
+		String id,
+		UserStatus currentUserStatus
+		) {}
