25e9e75b267ae2bf8b282fa2df7c2312331a916d
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-28 20:41:39 -0300

    refactor: change user status context query

--- src/main/java/br/com/omnirent/user/UserQueryRepository.java
+++ src/main/java/br/com/omnirent/user/UserQueryRepository.java
@@ -1,10 +1,13 @@
 package br.com.omnirent.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
+import br.com.omnirent.user.context.ChangeUserStatusContext;
 import br.com.omnirent.user.context.UserTakenContext;
 import br.com.omnirent.user.domain.User;
 
@@ -15,4 +18,10 @@ public interface UserQueryRepository extends Repository<User, String> {
 			FROM User u WHERE u.username = :username OR u.email = :email
 			""")
 	List<UserTakenContext> findTakenFields(String username, String email);
+	
+	@Query("""
+			SELECT new br.com.omnirent.user.context.ChangeUserStatusContext(u.id, u.userStatus)
+			FROM User u WHERE u.id = :id
+			""")
+	Optional<ChangeUserStatusContext> getUserStatusChangeContext(@Param("id") String userId); 
 }
