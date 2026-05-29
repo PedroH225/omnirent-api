b7458731c30d98f56e6b4964bb4c85dbffb80960
PedroH225 <pedrodguimaraes20@gmail.com>
2026-05-29 01:04:00 -0300

    refactor: update user query

--- src/main/java/br/com/omnirent/user/UserRepository.java
+++ src/main/java/br/com/omnirent/user/UserRepository.java
@@ -1,16 +1,13 @@
 package br.com.omnirent.user;
 
-import java.util.Optional;
+import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.omnirent.common.enums.UserStatus;
-import br.com.omnirent.security.context.LoginContext;
-import br.com.omnirent.user.domain.AuthMetadata;
 import br.com.omnirent.user.domain.User;
 
 @Repository
@@ -22,5 +19,13 @@ public interface UserRepository extends JpaRepository<User, String> {
 			WHERE u.id = :id AND u.userStatus = :currentStatus
 			""")
 	int updateUserStatus(String id, UserStatus currentStatus, UserStatus targetStatus);
+	
+	@Modifying
+	@Query("""
+			UPDATE User u SET u.name = :name, u.username = :username, u.email = :email,
+			u.birthDate = :birthDate
+			WHERE u.id = :id
+			""")
+	int updateUser(String id, String name, String username, String email, LocalDate birthDate);
 	
 }
