5b52193e95be5c9a63c619b9ca5092b27978eb88
PedroH225 <pedrodguimaraes20@gmail.com>
2026-06-03 16:48:01 -0300

    refactor: global logging event listener

--- src/main/java/br/com/omnirent/common/event/LoggingEventListener.java
+++ src/main/java/br/com/omnirent/common/event/LoggingEventListener.java
@@ -1,20 +1,15 @@
 package br.com.omnirent.common.event;
 
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import br.com.omnirent.address.event.AddressUpdatedEvent;
-import br.com.omnirent.common.enums.DomainEventType;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class LoggingEventListener {
-		
-	private void logDefaultEvent(DomainEventType event, String actorId, String entityId, Instant ocurredAt) {
+    @EventListener
+    public void handle(DomainEvent event) {
     	log.info("""
         		
 	        	event= {}
@@ -22,12 +17,6 @@ public class LoggingEventListener {
 	        	targetEntityId= {}
 	        	occurredAt= {}
 	        """,
-	        event, actorId, entityId, ocurredAt);
-	}
-
-    @EventListener
-    public void handle(AddressUpdatedEvent event) {
-    	logDefaultEvent(event.eventType(), event.actorId(), event.addressId(), event.occurredAt());
-    }
-
+	        event.eventType(), event.actorId(), event.entityId(), event.occurredAt());
+    	}
 }
