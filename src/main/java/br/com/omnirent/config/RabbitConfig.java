c69e0851d4282abfc5edfca708a84a9baac2e961
PedroH225 <pedrodguimaraes20@gmail.com>
2026-06-13 21:36:17 -0300

    chore: rabbitMQ config

---
+++ src/main/java/br/com/omnirent/config/RabbitConfig.java
@@ -0,0 +1,26 @@
+package br.com.omnirent.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    MessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+    
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter messageConverter) {
+
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+}
