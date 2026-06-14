d1112558201e95e9ee600834814c77843e1905bf
PedroH225 <pedrodguimaraes20@gmail.com>
2026-06-14 20:38:20 -0300

    refactor: omnirent personal name

--- src/main/java/br/com/omnirent/email/SMTEmailSender.java
+++ src/main/java/br/com/omnirent/email/SMTEmailSender.java
@@ -1,11 +1,15 @@
 package br.com.omnirent.email;
 
+import java.io.UnsupportedEncodingException;
+
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import br.com.omnirent.exception.infrastructure.EmailDeliveryException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,17 +22,28 @@ public class SMTEmailSender implements EmailSender {
 
 	@Override
 	public void send(EmailMessage message) {
-		SimpleMailMessage emailMessage = new SimpleMailMessage();
-		
-		emailMessage.setFrom(emailProperties.from());
-		emailMessage.setTo(message.to());
-		emailMessage.setSubject(message.subject());
-		emailMessage.setText(message.body() + message.footer());
-		
 		try {
-			javaMailSender.send(emailMessage);
-		} catch (MailException e) {
-			throw new EmailDeliveryException(e);
+		    MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+		    MimeMessageHelper helper =
+		            new MimeMessageHelper(mimeMessage, "UTF-8");
+
+		    helper.setFrom(
+		            emailProperties.from(),
+		            "OmniRent"
+		    );
+
+		    helper.setTo(message.to());
+		    helper.setSubject(message.subject());
+		    helper.setText(message.body() + message.footer());
+
+		    javaMailSender.send(mimeMessage);
+
+		} catch (MessagingException |
+		         UnsupportedEncodingException |
+		         MailException e) {
+
+		    throw new EmailDeliveryException(e);
 		}
 	}
 }
