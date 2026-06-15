ca464c389551699c077c8b322b8891787949bc8c
PedroH225 <pedrodguimaraes20@gmail.com>
2026-06-15 01:30:30 -0300

    fix: missing rental email service call

--- src/main/java/br/com/omnirent/email/EmailConsumer.java
+++ src/main/java/br/com/omnirent/email/EmailConsumer.java
@@ -37,6 +37,6 @@ public class EmailConsumer {
     @RabbitHandler
     public void handle(RentalCreatedEvent event) {
     	rentalEmailService.sendRentalCreatedToOwner(event);
-    	rentalEmailService.sendRentalCreatedToUser(event);
+    	rentalEmailService.sendRentalCreatedToUser (event);
     }
 }
