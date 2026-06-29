ead81037fb2ffe13cd51eacc3d4175bcd62148cc
PedroH225 <pedrodguimaraes20@gmail.com>
2026-06-29 02:56:48 -0300

    refactor: create payment method

--- src/main/java/br/com/omnirent/payment/model/Payment.java
+++ src/main/java/br/com/omnirent/payment/model/Payment.java
@@ -3,9 +3,15 @@ package br.com.omnirent.payment.model;
 import java.math.BigDecimal;
 import java.time.Instant;
 
+import com.stripe.model.PaymentMethod;
+
 import br.com.omnirent.common.enums.PaymentStatus;
-import br.com.omnirent.exception.infrastructure.InvalidPaymentStateTransitionException;
+import br.com.omnirent.exception.domain.InvalidPaymentStateTransitionException;
+import br.com.omnirent.exception.domain.PaymentReferenceAlreadyLockedException;
+import br.com.omnirent.payment.enums.PaymentProvider;
+import br.com.omnirent.payment.event.PaymentRequestedEvent;
 import br.com.omnirent.rental.domain.Rental;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -18,11 +24,12 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "payments")
-@Data
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment {
 
@@ -30,9 +37,12 @@ public class Payment {
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private String id;
 	
-	@ManyToOne(optional = false)
-	@JoinColumn(name = "rental_id", nullable = false)
+	@ManyToOne
+	@JoinColumn(name = "rental_id", insertable=false, updatable=false)
 	private Rental rental;
+	
+	@Column(name = "rental_id")
+	private String rentalId;
 	
 	@Embedded
 	private ExternalPaymentReference externalReference;;
@@ -52,6 +62,27 @@ public class Payment {
 	    }
 
 	    this.status = target;
+	}
+	
+	public static Payment create(String rentalId, BigDecimal amount, String currency) {
+	    Payment payment = new Payment();
+
+	    payment.rentalId = rentalId;
+	    payment.amount = amount;
+	    payment.currency = currency;
+	    payment.status = PaymentStatus.PENDING;
+	    payment.paidAt = null;
+	    
+	    return payment;
+	}
+	
+	public void attachExternalReference(PaymentProvider provider, String externalPaymentId) {
+	    if (this.status != PaymentStatus.PENDING) {
+	        throw new PaymentReferenceAlreadyLockedException();
+	    }
+	    
+		this.externalReference = 
+	    		new ExternalPaymentReference(provider, externalPaymentId);
 	}
 	
     public void markAsPaid(Instant paidAt) {
