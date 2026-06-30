d739bf0a3c6c3a3be71d50ecc889302fa701f11e
PedroH225 <pedrodguimaraes20@gmail.com>
2026-06-30 18:27:39 -0300

    refactor: optimistic lock exception

---
+++ src/main/java/br/com/omnirent/exception/domain/OptimisticLockException.java
@@ -0,0 +1,13 @@
+package br.com.omnirent.exception.domain;
+
+import br.com.omnirent.exception.common.DomainException;
+
+public class OptimisticLockException extends DomainException {
+	private static final long serialVersionUID = 1L;
+
+	public OptimisticLockException(String className, String id) {
+		super(String.format("%s was modified before update, id: %s",
+				className, id));
+	}
+
+}
