b35252901ce5aa345f01010f29cf0b84ccb854dc
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-04 01:42:27 -0300

    fix: remove cors config from springIT class

--- src/test/java/br/com/omnirent/integration/SpringIntegrationTest.java
+++ src/test/java/br/com/omnirent/integration/SpringIntegrationTest.java
@@ -1,12 +1,8 @@
 package br.com.omnirent.integration;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-
-import br.com.omnirent.config.SecurityTestConfig;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@Import(SecurityTestConfig.class)
 public abstract class SpringIntegrationTest extends IntegrationTest {
 
 }
