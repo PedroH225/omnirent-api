6a5d54dd2e33c7447df360a463b293527a3888ab
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-05 23:29:09 -0300

    refactor: switch payment mapper package

---
+++ src/main/java/br/com/omnirent/payment/PaymentMapper.java
@@ -0,0 +1,21 @@
+package br.com.omnirent.payment;
+
+import org.springframework.stereotype.Component;
+
+import br.com.omnirent.payment.context.audit.PaymentCreatedAuditSnapshot;
+import br.com.omnirent.payment.model.ExternalPaymentReference;
+import br.com.omnirent.payment.model.Payment;
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class PaymentMapper {
+
+	public PaymentCreatedAuditSnapshot toCreatedSnapshot(Payment payment) {
+		ExternalPaymentReference externalReference = payment.getExternalReference();
+		return new PaymentCreatedAuditSnapshot(
+				payment.getId(), payment.getRentalId(), payment.getStatus(),
+				externalReference.getPaymentProvider(), payment.getAmount(),
+				payment.getCurrency(), externalReference.getExternalPaymentId());
+	}
+}
