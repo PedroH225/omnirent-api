0b07f97aa5601cc92ad2a7baa011f786fcfe2834
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-07 19:41:41 -0300

    refactor: external identity boiler plate

--- src/main/java/br/com/omnirent/security/domain/ExternalIdentity.java
+++ src/main/java/br/com/omnirent/security/domain/ExternalIdentity.java
@@ -9,13 +9,17 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "user_identities")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExternalIdentity extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 	
