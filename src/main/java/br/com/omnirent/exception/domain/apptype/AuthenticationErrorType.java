2c1aa817bdcc476c2fa700f3592e82f5fd54ab70
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-09 02:26:29 -0300

    refactor: oauth2 error types

--- src/main/java/br/com/omnirent/exception/domain/apptype/AuthenticationErrorType.java
+++ src/main/java/br/com/omnirent/exception/domain/apptype/AuthenticationErrorType.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 @Getter
 public enum AuthenticationErrorType implements AppErrorType {
 
+	OAUTH_AUTHENTICATION_FAILED("UNAUTHORIZED", "OAUTH_AUTHENTICATION_FAILED", "oauth_authentication_failed", HttpStatus.UNAUTHORIZED),
+    OAUTH_PROVIDER_UNAVAILABLE("SERVICE_UNAVAILABLE", "OAUTH_PROVIDER_UNAVAILABLE", "oauth_unavailable", HttpStatus.SERVICE_UNAVAILABLE),
+	OAUTH_ACCESS_DENIED("UNAUTHORIZED", "OAUTH_ACCESS_DENIED", "oauth_access_denied", HttpStatus.UNAUTHORIZED),
     INVALID_TOKEN("UNAUTHORIZED", "INVALID_TOKEN", "authentication_required", HttpStatus.UNAUTHORIZED),
     AUTHENTICATION_REQUIRED("UNAUTHORIZED", "AUTHENTICATION_REQUIRED", "authentication_required", HttpStatus.UNAUTHORIZED),
     INVALID_CREDENTIALS("UNAUTHORIZED", "INVALID_CREDENTIALS", "invalid_credentials", HttpStatus.UNAUTHORIZED);
