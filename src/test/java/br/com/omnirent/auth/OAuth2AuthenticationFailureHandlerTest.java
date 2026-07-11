f2fc2525a2eba486582d014273f127590887747f
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-10 21:46:20 -0300

    test: oauth provider unavailable

--- src/test/java/br/com/omnirent/auth/OAuth2AuthenticationFailureHandlerTest.java
+++ src/test/java/br/com/omnirent/auth/OAuth2AuthenticationFailureHandlerTest.java
@@ -78,4 +78,20 @@ public class OAuth2AuthenticationFailureHandlerTest {
         	.isEqualTo(AuthenticationErrorType.OAUTH_ACCESS_DENIED.getErrorType());
 		verify(response).sendRedirect("http://localhost:3000/login?error=" + captor.getValue().getErrorCode());
 	}
+	
+	@Test
+	void shouldHandleOAuthProviderUnavailable() throws IOException, ServletException {
+		OAuth2Error error = new OAuth2Error("server_error");
+		OAuth2AuthenticationException ex = new OAuth2AuthenticationException(error);
+		when(appProperties.frontUrl()).thenReturn("http://localhost:3000");
+
+		failureHandler.onAuthenticationFailure(request, response, ex);
+
+		ArgumentCaptor<ApiException> captor = ArgumentCaptor.forClass(ApiException.class);
+		verify(apiWriter).onApiError(eq(request), eq(response), captor.capture());
+
+		assertThat(captor.getValue().getErrorType())
+			.isEqualTo(AuthenticationErrorType.OAUTH_PROVIDER_UNAVAILABLE.getErrorType());
+		verify(response).sendRedirect("http://localhost:3000/login?error=" + captor.getValue().getErrorCode());
+	}
 }
