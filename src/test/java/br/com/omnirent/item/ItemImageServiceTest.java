00fd89a892d8c5ad34237a0bcee08b4dfa44926d
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-15 17:34:58 -0300

    test: unsupported format

--- src/test/java/br/com/omnirent/item/ItemImageServiceTest.java
+++ src/test/java/br/com/omnirent/item/ItemImageServiceTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
@@ -483,6 +484,19 @@ public class ItemImageServiceTest {
 
         assertEquals(
                 ImageErrorType.INVALID_IMAGE.getErrorCode(),
+                exception.getErrorCode()
+        );
+    }
+    
+    @Test
+    void getValidBufferedImage_throwsExceptionWhenFormatIsUnsupported() throws IOException {
+        MultipartFile file = MultipartFileTestFactory.image("image/tiff", "tiff");
+
+        ApiException exception = assertThrows(ApiException.class, () ->
+                imageService.getValidBufferedImage(file));
+
+        assertEquals(
+                ImageErrorType.UNSUPPORTED_MEDIA_TYPE.getErrorCode(),
                 exception.getErrorCode()
         );
     }
