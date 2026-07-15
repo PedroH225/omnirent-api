08bdcbf19fa018cfe0adca24a12160848fc6d5f7
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-15 20:50:16 -0300

    refactor: aws exception types and messages

---
+++ src/main/java/br/com/omnirent/exception/domain/apptype/StorageErrorType.java
@@ -0,0 +1,36 @@
+package br.com.omnirent.exception.domain.apptype;
+
+import org.springframework.http.HttpStatus;
+
+import br.com.omnirent.exception.common.AppErrorType;
+import lombok.Getter;
+
+@Getter
+public enum StorageErrorType implements AppErrorType {
+
+	STORAGE_UNAVAILABLE("STORAGE_UNAVAILABLE", "STORAGE_UNAVAILABLE", "storage.unavailable", HttpStatus.SERVICE_UNAVAILABLE),
+	STORAGE_UPLOAD_FAILED("STORAGE_UPLOAD_FAILED", "STORAGE_UPLOAD_FAILED", "storage.upload.failed", HttpStatus.INTERNAL_SERVER_ERROR),
+	STORAGE_ACCESS_DENIED("STORAGE_ACCESS_DENIED", "STORAGE_ACCESS_DENIED", "storage.access.denied", HttpStatus.FORBIDDEN),
+	STORAGE_RATE_LIMITED("STORAGE_RATE_LIMITED", "STORAGE_RATE_LIMITED", "storage.rate.limited", HttpStatus.TOO_MANY_REQUESTS);
+	
+    private String errorType;
+	
+	private String errorCode;
+	
+	private String messageKey;
+	
+	private HttpStatus httpCode;
+	
+	StorageErrorType(String errorType, String errorCode, String messageKey,
+			HttpStatus httpCode) {
+		this.errorType = errorType;
+		this.errorCode = errorCode;
+		this.messageKey = messageKey;
+		this.httpCode = httpCode;
+	}
+	
+	@Override
+	public String getMessageKey() {
+		return "error.storage." + this.messageKey;
+	}
+}
