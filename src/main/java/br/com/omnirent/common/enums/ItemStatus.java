ef6aff504fb47b232a8a4479ce1beb358ddba875
PedroH225 <pedrodguimaraes20@gmail.com>
2026-07-19 00:12:56 -0300

    feat: rented and analisys item status

--- src/main/java/br/com/omnirent/common/enums/ItemStatus.java
+++ src/main/java/br/com/omnirent/common/enums/ItemStatus.java
@@ -1,10 +1,12 @@
 package br.com.omnirent.common.enums;
 
 public enum ItemStatus {
+	ANALISYS,
 	AVAILABLE,
+	RENTED,
 	UNAVAILABLE,
 	BLOCKED;
-
+	
 	public String getMessageKey() {
 		return "item.status." + name();
 	}
